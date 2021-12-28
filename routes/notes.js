935b0ae2375e40947d740b7600daffb4bccc6aa0
gnotfound <guilhermelinharesr@gmail.com>
2021-12-27 21:03:22 -0300

    inserindo dados no banco

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,23 @@
 const Router = require("express").Router;
+const db = require("../db/connection");
+const { ObjectId } = require("mongodb");
 
 const router = Router();
 
 router.get("/", (req, res) => {
     res.render('notes/create');
 });
+
+router.post("/", (req, res) => {
+
+    const { title, description } = req.body;
+
+    db.getDb()
+    .db()
+    .collection("notes")
+    .insertOne({ title: title, description: description});
+
+    res.redirect("/");
+})
 
 module.exports = router;
